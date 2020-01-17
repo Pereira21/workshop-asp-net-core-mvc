848ef85f722806d00d334da182bf4b7077af100c
Lucas D'Amico <lucas.pereira@criaresistemas.com.br>
2020-01-17 13:25:37 -0300

    SellersService with Include and Details

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,7 +27,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(p => p.Id == id);
+            return _context.Seller.Include(obj => obj.Departamento).FirstOrDefault(p => p.Id == id);
         }
 
         public void Delete(int id)
